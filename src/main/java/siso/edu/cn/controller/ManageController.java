d62cac2313a7c3c51d64784410dacf7cbcf20e72
taowenyin <wenyin.tao@163.com>
2018-11-13 14:20:11 +0800

    完善APIDOC描述

--- src/main/java/siso/edu/cn/controller/ManageController.java
+++ src/main/java/siso/edu/cn/controller/ManageController.java
@@ -13,6 +13,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+/**
+ * @apiDefine group 管理接口
+ * @apiDefine version 0.0.1
+ */
 @RestController
 @RequestMapping(value = "/api/manage", produces = "application/json;charset=utf-8")
 public class ManageController {
@@ -57,14 +61,15 @@ public class ManageController {
     }
 
     /**
-     * @api {get} /department/:id 请求部门信息
+     * @api {get} /api/manage/department/:id 根据ID获取部门信息
      * @apiName getDepartmentById
-     * @apiGroup Department
+     * @apiGroup group
      *
      * @apiParam {Number} id 部门ID
      *
-     * @apiSuccess {String} firstname Firstname of the User.
-     * @apiSuccess {String} lastname  Lastname of the User.
+     * @apiSuccess {String} code 返回码.
+     * @apiSuccess {String} msg  返回消息.
+     * @apiSuccess {Object} data  JSON格式的对象.
      */
     @RequestMapping(value = "/department/{id}", method = RequestMethod.GET)
     public ResultEntity getDepartmentById(@PathVariable("id") long id) {

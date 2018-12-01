e2032a9467ded3c199c62a73014781e4d29b0829
taowenyin <wenyin.tao@163.com>
2018-12-01 10:00:27 +0800

    添加telnet重启服务器的指令

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -10,3 +10,20 @@ prompt
 bye
 EOF
 echo "deploy to server successfully"
+
+export $SERVER_USERNAME = "ubuntu"
+export $SERVER_PASSWORD = "Root12365$"
+export $SERVER_IP = "172.81.239.174"
+export $SERVER_CMD1 = "sudo service tomcat restart"
+export $SERVER_CMD2 = "exit"
+
+(
+    sleep 10;
+    echo $SERVER_USERNAME;
+    sleep 10;
+    echo $SERVER_PASSWORD;
+    sleep 10;
+    echo $SERVER_CMD1;
+    sleep 50;
+    echo $SERVER_CMD2;
+)|telnet $SERVER_IP

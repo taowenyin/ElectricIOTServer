75993ae458ae61f08ad41277b9000ec52a1a7302
taowenyin <wenyin.tao@163.com>
2018-12-01 11:11:08 +0800

    修改变量定义方式

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 export PUT_FILE=ElectricIOTServer.war
 export CURRENT_DIR=$PWD/build/libs/
+export SERVER_USERNAME="ubuntu"
+export SERVER_PASSWORD="Root12365$"
+export SERVER_IP="172.81.239.174"
+export SERVER_CMD1="sudo service tomcat restart"
+export SERVER_CMD2="exit"
+
 ftp -p -v -n 172.81.239.174<<EOF
 user ubuntu Root12365$
 cd /opt/tomcat/webapps
@@ -10,12 +16,6 @@ prompt
 bye
 EOF
 echo "deploy to server successfully"
-
-export SERVER_USERNAME = "ubuntu"
-export SERVER_PASSWORD = "Root12365$"
-export SERVER_IP = "172.81.239.174"
-export SERVER_CMD1 = "sudo service tomcat restart"
-export SERVER_CMD2 = "exit"
 
 (
     sleep 10;

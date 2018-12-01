25f52927f13fd11e7273a35519732d1c1e74bdf0
taowenyin <wenyin.tao@163.com>
2018-12-01 12:23:30 +0800

    修改DeviceCmdEntity和DeviceLocationEntity字段内容

--- src/main/java/siso/edu/cn/entity/DeviceCmdEntity.java
+++ src/main/java/siso/edu/cn/entity/DeviceCmdEntity.java
@@ -14,16 +14,42 @@ import javax.persistence.*;
 public class DeviceCmdEntity {
     @JsonProperty("id")
     private long id;
-    @JsonProperty("cmd")
-    private String cmd;
-    @JsonProperty("is_send")
-    private int isSend;
     @JsonProperty("device_id")
     private long deviceId;
+    @JsonProperty("get_imsi")
+    private Integer getImsi;
+    @JsonProperty("set_device_name")
+    private String setDeviceName;
+    @JsonProperty("get_device_name")
+    private Integer getDeviceName;
+    @JsonProperty("set_device_ip")
+    private String setDeviceIp;
+    @JsonProperty("set_device_port")
+    private Integer setDevicePort;
+    @JsonProperty("get_device_ip")
+    private Integer getDeviceIp;
+    @JsonProperty("get_device_port")
+    private Integer getDevicePort;
+    @JsonProperty("set_gps_record_interval")
+    private Integer setGpsRecordInterval;
+    @JsonProperty("get_gps_record_interval")
+    private Integer getGpsRecordInterval;
+    @JsonProperty("set_gps_send_interval")
+    private Integer setGpsSendInterval;
+    @JsonProperty("get_gps_send_interval")
+    private Integer getGpsSendInterval;
+    @JsonProperty("reset_device")
+    private Integer resetDevice;
+    @JsonProperty("set_power_tcp_live_interval")
+    private Integer setPowerTcpLiveInterval;
+    @JsonProperty("get_power_tcp_live_interval")
+    private Integer getPowerTcpLiveInterval;
     @JsonProperty("create_time")
     private String createTime;
     @JsonProperty("send_time")
     private String sendTime;
+    @JsonProperty("is_send")
+    private Integer isSend;
     @JsonIgnore
     private DeviceEntity deviceByDeviceId;
 
@@ -39,26 +65,6 @@ public class DeviceCmdEntity {
     }
 
     @Basic
-    @Column(name = "cmd", nullable = false)
-    public String getCmd() {
-        return cmd;
-    }
-
-    public void setCmd(String cmd) {
-        this.cmd = cmd;
-    }
-
-    @Basic
-    @Column(name = "is_send", nullable = false)
-    public int getIsSend() {
-        return isSend;
-    }
-
-    public void setIsSend(int isSend) {
-        this.isSend = isSend;
-    }
-
-    @Basic
     @Column(name = "device_id", nullable = false)
     public long getDeviceId() {
         return deviceId;
@@ -66,6 +72,146 @@ public class DeviceCmdEntity {
 
     public void setDeviceId(long deviceId) {
         this.deviceId = deviceId;
+    }
+
+    @Basic
+    @Column(name = "get_imsi", nullable = true)
+    public Integer getGetImsi() {
+        return getImsi;
+    }
+
+    public void setGetImsi(Integer getImsi) {
+        this.getImsi = getImsi;
+    }
+
+    @Basic
+    @Column(name = "set_device_name", nullable = true, length = 45)
+    public String getSetDeviceName() {
+        return setDeviceName;
+    }
+
+    public void setSetDeviceName(String setDeviceName) {
+        this.setDeviceName = setDeviceName;
+    }
+
+    @Basic
+    @Column(name = "get_device_name", nullable = true)
+    public Integer getGetDeviceName() {
+        return getDeviceName;
+    }
+
+    public void setGetDeviceName(Integer getDeviceName) {
+        this.getDeviceName = getDeviceName;
+    }
+
+    @Basic
+    @Column(name = "set_device_ip", nullable = true, length = 45)
+    public String getSetDeviceIp() {
+        return setDeviceIp;
+    }
+
+    public void setSetDeviceIp(String setDeviceIp) {
+        this.setDeviceIp = setDeviceIp;
+    }
+
+    @Basic
+    @Column(name = "set_device_port", nullable = true)
+    public Integer getSetDevicePort() {
+        return setDevicePort;
+    }
+
+    public void setSetDevicePort(Integer setDevicePort) {
+        this.setDevicePort = setDevicePort;
+    }
+
+    @Basic
+    @Column(name = "get_device_ip", nullable = true)
+    public Integer getGetDeviceIp() {
+        return getDeviceIp;
+    }
+
+    public void setGetDeviceIp(Integer getDeviceIp) {
+        this.getDeviceIp = getDeviceIp;
+    }
+
+    @Basic
+    @Column(name = "get_device_port", nullable = true)
+    public Integer getGetDevicePort() {
+        return getDevicePort;
+    }
+
+    public void setGetDevicePort(Integer getDevicePort) {
+        this.getDevicePort = getDevicePort;
+    }
+
+    @Basic
+    @Column(name = "set_gps_record_interval", nullable = true)
+    public Integer getSetGpsRecordInterval() {
+        return setGpsRecordInterval;
+    }
+
+    public void setSetGpsRecordInterval(Integer setGpsRecordInterval) {
+        this.setGpsRecordInterval = setGpsRecordInterval;
+    }
+
+    @Basic
+    @Column(name = "get_gps_record_interval", nullable = true)
+    public Integer getGetGpsRecordInterval() {
+        return getGpsRecordInterval;
+    }
+
+    public void setGetGpsRecordInterval(Integer getGpsRecordInterval) {
+        this.getGpsRecordInterval = getGpsRecordInterval;
+    }
+
+    @Basic
+    @Column(name = "set_gps_send_interval", nullable = true)
+    public Integer getSetGpsSendInterval() {
+        return setGpsSendInterval;
+    }
+
+    public void setSetGpsSendInterval(Integer setGpsSendInterval) {
+        this.setGpsSendInterval = setGpsSendInterval;
+    }
+
+    @Basic
+    @Column(name = "get_gps_send_interval", nullable = true)
+    public Integer getGetGpsSendInterval() {
+        return getGpsSendInterval;
+    }
+
+    public void setGetGpsSendInterval(Integer getGpsSendInterval) {
+        this.getGpsSendInterval = getGpsSendInterval;
+    }
+
+    @Basic
+    @Column(name = "reset_device", nullable = true)
+    public Integer getResetDevice() {
+        return resetDevice;
+    }
+
+    public void setResetDevice(Integer resetDevice) {
+        this.resetDevice = resetDevice;
+    }
+
+    @Basic
+    @Column(name = "set_power_tcp_live_interval", nullable = true)
+    public Integer getSetPowerTcpLiveInterval() {
+        return setPowerTcpLiveInterval;
+    }
+
+    public void setSetPowerTcpLiveInterval(Integer setPowerTcpLiveInterval) {
+        this.setPowerTcpLiveInterval = setPowerTcpLiveInterval;
+    }
+
+    @Basic
+    @Column(name = "get_power_tcp_live_interval", nullable = true)
+    public Integer getGetPowerTcpLiveInterval() {
+        return getPowerTcpLiveInterval;
+    }
+
+    public void setGetPowerTcpLiveInterval(Integer getPowerTcpLiveInterval) {
+        this.getPowerTcpLiveInterval = getPowerTcpLiveInterval;
     }
 
     @Basic
@@ -88,6 +234,16 @@ public class DeviceCmdEntity {
         this.sendTime = sendTime;
     }
 
+    @Basic
+    @Column(name = "is_send", nullable = true)
+    public Integer getIsSend() {
+        return isSend;
+    }
+
+    public void setIsSend(Integer isSend) {
+        this.isSend = isSend;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -96,11 +252,34 @@ public class DeviceCmdEntity {
         DeviceCmdEntity that = (DeviceCmdEntity) o;
 
         if (id != that.id) return false;
-        if (isSend != that.isSend) return false;
         if (deviceId != that.deviceId) return false;
-        if (cmd != null ? !cmd.equals(that.cmd) : that.cmd != null) return false;
+        if (getImsi != null ? !getImsi.equals(that.getImsi) : that.getImsi != null) return false;
+        if (setDeviceName != null ? !setDeviceName.equals(that.setDeviceName) : that.setDeviceName != null)
+            return false;
+        if (getDeviceName != null ? !getDeviceName.equals(that.getDeviceName) : that.getDeviceName != null)
+            return false;
+        if (setDeviceIp != null ? !setDeviceIp.equals(that.setDeviceIp) : that.setDeviceIp != null) return false;
+        if (setDevicePort != null ? !setDevicePort.equals(that.setDevicePort) : that.setDevicePort != null)
+            return false;
+        if (getDeviceIp != null ? !getDeviceIp.equals(that.getDeviceIp) : that.getDeviceIp != null) return false;
+        if (getDevicePort != null ? !getDevicePort.equals(that.getDevicePort) : that.getDevicePort != null)
+            return false;
+        if (setGpsRecordInterval != null ? !setGpsRecordInterval.equals(that.setGpsRecordInterval) : that.setGpsRecordInterval != null)
+            return false;
+        if (getGpsRecordInterval != null ? !getGpsRecordInterval.equals(that.getGpsRecordInterval) : that.getGpsRecordInterval != null)
+            return false;
+        if (setGpsSendInterval != null ? !setGpsSendInterval.equals(that.setGpsSendInterval) : that.setGpsSendInterval != null)
+            return false;
+        if (getGpsSendInterval != null ? !getGpsSendInterval.equals(that.getGpsSendInterval) : that.getGpsSendInterval != null)
+            return false;
+        if (resetDevice != null ? !resetDevice.equals(that.resetDevice) : that.resetDevice != null) return false;
+        if (setPowerTcpLiveInterval != null ? !setPowerTcpLiveInterval.equals(that.setPowerTcpLiveInterval) : that.setPowerTcpLiveInterval != null)
+            return false;
+        if (getPowerTcpLiveInterval != null ? !getPowerTcpLiveInterval.equals(that.getPowerTcpLiveInterval) : that.getPowerTcpLiveInterval != null)
+            return false;
         if (createTime != null ? !createTime.equals(that.createTime) : that.createTime != null) return false;
         if (sendTime != null ? !sendTime.equals(that.sendTime) : that.sendTime != null) return false;
+        if (isSend != null ? !isSend.equals(that.isSend) : that.isSend != null) return false;
 
         return true;
     }
@@ -108,11 +287,24 @@ public class DeviceCmdEntity {
     @Override
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (cmd != null ? cmd.hashCode() : 0);
-        result = 31 * result + (int) isSend;
         result = 31 * result + (int) (deviceId ^ (deviceId >>> 32));
+        result = 31 * result + (getImsi != null ? getImsi.hashCode() : 0);
+        result = 31 * result + (setDeviceName != null ? setDeviceName.hashCode() : 0);
+        result = 31 * result + (getDeviceName != null ? getDeviceName.hashCode() : 0);
+        result = 31 * result + (setDeviceIp != null ? setDeviceIp.hashCode() : 0);
+        result = 31 * result + (setDevicePort != null ? setDevicePort.hashCode() : 0);
+        result = 31 * result + (getDeviceIp != null ? getDeviceIp.hashCode() : 0);
+        result = 31 * result + (getDevicePort != null ? getDevicePort.hashCode() : 0);
+        result = 31 * result + (setGpsRecordInterval != null ? setGpsRecordInterval.hashCode() : 0);
+        result = 31 * result + (getGpsRecordInterval != null ? getGpsRecordInterval.hashCode() : 0);
+        result = 31 * result + (setGpsSendInterval != null ? setGpsSendInterval.hashCode() : 0);
+        result = 31 * result + (getGpsSendInterval != null ? getGpsSendInterval.hashCode() : 0);
+        result = 31 * result + (resetDevice != null ? resetDevice.hashCode() : 0);
+        result = 31 * result + (setPowerTcpLiveInterval != null ? setPowerTcpLiveInterval.hashCode() : 0);
+        result = 31 * result + (getPowerTcpLiveInterval != null ? getPowerTcpLiveInterval.hashCode() : 0);
         result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
         result = 31 * result + (sendTime != null ? sendTime.hashCode() : 0);
+        result = 31 * result + (isSend != null ? isSend.hashCode() : 0);
         return result;
     }
 
--- src/main/java/siso/edu/cn/entity/DeviceLocationEntity.java
+++ src/main/java/siso/edu/cn/entity/DeviceLocationEntity.java
@@ -15,14 +15,30 @@ import java.math.BigDecimal;
 public class DeviceLocationEntity {
     @JsonProperty("id")
     private long id;
+    @JsonProperty("device_id")
+    private long deviceId;
+    @JsonProperty("record_time")
+    private String recordTime;
+    @JsonProperty("signal_quality")
+    private int signalQuality;
+    @JsonProperty("device_on_off")
+    private int deviceOnOff;
+    @JsonProperty("battery_voltage")
+    private int batteryVoltage;
     @JsonProperty("longitude")
     private BigDecimal longitude;
     @JsonProperty("latitude")
     private BigDecimal latitude;
-    @JsonProperty("record_time")
-    private String recordTime;
-    @JsonProperty("device_id")
-    private long deviceId;
+    @JsonProperty("nation_num")
+    private Integer nationNum;
+    @JsonProperty("mobile_num")
+    private Integer mobileNum;
+    @JsonProperty("location_num")
+    private Integer locationNum;
+    @JsonProperty("community_num")
+    private String communityNum;
+    @JsonProperty("station_num")
+    private String stationNum;
     @JsonIgnore
     private DeviceEntity deviceByDeviceId;
 
@@ -38,23 +54,13 @@ public class DeviceLocationEntity {
     }
 
     @Basic
-    @Column(name = "longitude", nullable = false, precision = 6)
-    public BigDecimal getLongitude() {
-        return longitude;
+    @Column(name = "device_id", nullable = false)
+    public long getDeviceId() {
+        return deviceId;
     }
 
-    public void setLongitude(BigDecimal longitude) {
-        this.longitude = longitude;
-    }
-
-    @Basic
-    @Column(name = "latitude", nullable = false, precision = 6)
-    public BigDecimal getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(BigDecimal latitude) {
-        this.latitude = latitude;
+    public void setDeviceId(long deviceId) {
+        this.deviceId = deviceId;
     }
 
     @Basic
@@ -68,13 +74,103 @@ public class DeviceLocationEntity {
     }
 
     @Basic
-    @Column(name = "device_id", nullable = false)
-    public long getDeviceId() {
-        return deviceId;
+    @Column(name = "signal_quality", nullable = false)
+    public int getSignalQuality() {
+        return signalQuality;
     }
 
-    public void setDeviceId(long deviceId) {
-        this.deviceId = deviceId;
+    public void setSignalQuality(int signalQuality) {
+        this.signalQuality = signalQuality;
+    }
+
+    @Basic
+    @Column(name = "device_on_off", nullable = false)
+    public int getDeviceOnOff() {
+        return deviceOnOff;
+    }
+
+    public void setDeviceOnOff(int deviceOnOff) {
+        this.deviceOnOff = deviceOnOff;
+    }
+
+    @Basic
+    @Column(name = "battery_voltage", nullable = false)
+    public int getBatteryVoltage() {
+        return batteryVoltage;
+    }
+
+    public void setBatteryVoltage(int batteryVoltage) {
+        this.batteryVoltage = batteryVoltage;
+    }
+
+    @Basic
+    @Column(name = "longitude", nullable = true, precision = 6)
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    @Basic
+    @Column(name = "latitude", nullable = true, precision = 6)
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    @Basic
+    @Column(name = "nation_num", nullable = true)
+    public Integer getNationNum() {
+        return nationNum;
+    }
+
+    public void setNationNum(Integer nationNum) {
+        this.nationNum = nationNum;
+    }
+
+    @Basic
+    @Column(name = "mobile_num", nullable = true)
+    public Integer getMobileNum() {
+        return mobileNum;
+    }
+
+    public void setMobileNum(Integer mobileNum) {
+        this.mobileNum = mobileNum;
+    }
+
+    @Basic
+    @Column(name = "location_num", nullable = true)
+    public Integer getLocationNum() {
+        return locationNum;
+    }
+
+    public void setLocationNum(Integer locationNum) {
+        this.locationNum = locationNum;
+    }
+
+    @Basic
+    @Column(name = "community_num", nullable = true, length = 45)
+    public String getCommunityNum() {
+        return communityNum;
+    }
+
+    public void setCommunityNum(String communityNum) {
+        this.communityNum = communityNum;
+    }
+
+    @Basic
+    @Column(name = "station_num", nullable = true, length = 45)
+    public String getStationNum() {
+        return stationNum;
+    }
+
+    public void setStationNum(String stationNum) {
+        this.stationNum = stationNum;
     }
 
     @Override
@@ -86,9 +182,17 @@ public class DeviceLocationEntity {
 
         if (id != that.id) return false;
         if (deviceId != that.deviceId) return false;
+        if (signalQuality != that.signalQuality) return false;
+        if (deviceOnOff != that.deviceOnOff) return false;
+        if (batteryVoltage != that.batteryVoltage) return false;
+        if (recordTime != null ? !recordTime.equals(that.recordTime) : that.recordTime != null) return false;
         if (longitude != null ? !longitude.equals(that.longitude) : that.longitude != null) return false;
         if (latitude != null ? !latitude.equals(that.latitude) : that.latitude != null) return false;
-        if (recordTime != null ? !recordTime.equals(that.recordTime) : that.recordTime != null) return false;
+        if (nationNum != null ? !nationNum.equals(that.nationNum) : that.nationNum != null) return false;
+        if (mobileNum != null ? !mobileNum.equals(that.mobileNum) : that.mobileNum != null) return false;
+        if (locationNum != null ? !locationNum.equals(that.locationNum) : that.locationNum != null) return false;
+        if (communityNum != null ? !communityNum.equals(that.communityNum) : that.communityNum != null) return false;
+        if (stationNum != null ? !stationNum.equals(that.stationNum) : that.stationNum != null) return false;
 
         return true;
     }
@@ -96,10 +200,18 @@ public class DeviceLocationEntity {
     @Override
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (deviceId ^ (deviceId >>> 32));
+        result = 31 * result + (recordTime != null ? recordTime.hashCode() : 0);
+        result = 31 * result + signalQuality;
+        result = 31 * result + deviceOnOff;
+        result = 31 * result + batteryVoltage;
         result = 31 * result + (longitude != null ? longitude.hashCode() : 0);
         result = 31 * result + (latitude != null ? latitude.hashCode() : 0);
-        result = 31 * result + (recordTime != null ? recordTime.hashCode() : 0);
-        result = 31 * result + (int) (deviceId ^ (deviceId >>> 32));
+        result = 31 * result + (nationNum != null ? nationNum.hashCode() : 0);
+        result = 31 * result + (mobileNum != null ? mobileNum.hashCode() : 0);
+        result = 31 * result + (locationNum != null ? locationNum.hashCode() : 0);
+        result = 31 * result + (communityNum != null ? communityNum.hashCode() : 0);
+        result = 31 * result + (stationNum != null ? stationNum.hashCode() : 0);
         return result;
     }
 

fd2699e14f209b9e052bd1c22fbd7fd87d7606ca
wenyin.tao <wenyin.tao@163.com>
2019-09-15 15:15:34 +0800

    添加通用指令接口函数

--- src/main/java/siso/edu/cn/controller/DeviceController.java
+++ src/main/java/siso/edu/cn/controller/DeviceController.java
@@ -422,4 +422,78 @@ public class DeviceController extends IControllerImpl {
         return this.createResultEntity(ResultEntity.NOT_FIND_ERROR);
     }
 
+    /**
+     * @api {post} /api/manage/device 创建新设备
+     * @apiVersion 0.0.1
+     * @apiName createDevice
+     * @apiGroup deviceGroup
+     *
+     * @apiParam {String} imsi 手机卡IMSI
+     * @apiParam {String} [uid] 自定义ID
+     * @apiParam {String} [name] 设备名称
+     * @apiParam {String} [serial_number] 设备序列号
+     * @apiParam {Number} [type_id] 设备类型ID
+     * @apiParam {Number} [status_id] 设备状态ID
+     * @apiParam {Number} [user_id] 保管员用户ID
+     * @apiParam {Number} [department_id] 设备所属部门ID
+     * @apiParam {String} [comment] 设备说明
+     * @apiParam {Number} [keep_live_interval=60] 设备心跳间隔（单位：秒）
+     * @apiParam {Number} [battery_sleep_time=180] 电源供电时的休眠时间（单位：分钟）
+     * @apiParam {Number} [battery_keep_live_time=300] 电池供电时心跳包发送后保持连接的时间（单位：秒）
+     *
+     * @apiSuccess {String} code 返回码.
+     * @apiSuccess {String} msg  返回消息.
+     * @apiSuccess {Object} data  JSON格式的对象.
+     */
+
+    /**
+     * @api {post} /api/manage/device/command 向设备添加通用指令
+     * @apiVersion 0.0.1
+     * @apiName createCommand
+     * @apiGroup deviceGroup
+     *
+     * @apiParam {Number} id 设备ID
+     * @apiParam {String} command  指令内容
+     *
+     * @apiSuccess {String} code 返回码.
+     * @apiSuccess {String} msg  返回消息.
+     * @apiSuccess {Object} data  JSON格式的对象.
+     */
+    @RequestMapping(value = "/device/command", method = RequestMethod.POST)
+    public ResultEntity createCommand(@RequestParam("id") long id, @RequestParam(name = "command") String command) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 获取时间格式
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+
+        // 创建指令实体对象
+        DeviceCmdEntity deviceCmdEntity = new DeviceCmdEntity();
+
+        DeviceEntity entity = deviceService.findById(id);
+        if (entity == null) {
+            return this.createResultEntity(ResultEntity.NOT_FIND_ERROR);
+        }
+
+        // 设置设备指令的ID
+        deviceCmdEntity.setDeviceId(id);
+        // 设置设备指令的创建时间
+        deviceCmdEntity.setCreateTime(simpleDateFormat.format(new Date()));
+
+        if (!command.isEmpty()) {
+            // 设置设备通用指令
+            deviceCmdEntity.setSetCommand(command);
+        }
+
+        // 保存指令
+        deviceCmdService.save(deviceCmdEntity);
+        if (deviceCmdEntity.getId() > 0) {
+            System.out.println("===Save Device CMD OK ===");
+        } else {
+            System.out.println("===Save Device CMD Fail ===");
+        }
+
+        return this.createResultEntity(ResultEntity.SUCCESS, objectMapper.convertValue(deviceCmdEntity, JsonNode.class));
+    }
+
 }

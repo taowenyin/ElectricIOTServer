b29519eafc079d8945937b3f0c0fe3d0f537bb5d
wenyin.tao <wenyin.tao@163.com>
2019-09-15 14:54:47 +0800

    修改数据表字段属性

--- src/main/java/siso/edu/cn/entity/DeviceCmdEntity.java
+++ src/main/java/siso/edu/cn/entity/DeviceCmdEntity.java
@@ -265,7 +265,7 @@ public class DeviceCmdEntity {
     }
 
     @ManyToOne
-    @JoinColumn(name = "device_id", referencedColumnName = "id", nullable = false)
+    @JoinColumn(name = "device_id", referencedColumnName = "id", nullable = false, insertable = false, updatable = false)
     public DeviceEntity getDeviceByDeviceId() {
         return deviceByDeviceId;
     }
